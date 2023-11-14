7ccb7e73ccd9d5856e4a936596cbafd2c21baff5
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-11-14 16:19:07 -0500

    added supabase hook

--- sanity/actions/CustomPublish.ts
+++ sanity/actions/CustomPublish.ts
@@ -1,16 +1,38 @@
+import { supabase } from "@/supabase/lib/supabaseClient";
+
 export function CustomPublish(originalPublishAction: any) {
     const BetterAction = (props: any) => {
-      const originalResult = originalPublishAction(props)
-      return {
-        ...originalResult,
-        onHandle: () => {
-          // Add our custom functionality
-          // console.log('Hello world!')
-          alert("Hello world")
-          // then delegate to original handler
-          originalResult.onHandle()
-        },
-      }
+        const originalResult = originalPublishAction(props);
+
+        if (props.type == "products") {
+            return {
+                ...originalResult,
+                onHandle: () => {
+                    // custom function for supabase will go here
+                    console.log(props.draft.price)
+                    supabase.from('products')
+                      .upsert({ sanity_id: props.id, title: props.draft.title, price: props.draft.price, }, { onConflict: 'sanity_id' })
+                      .then(response =>  {
+                          console.log(response)
+
+                          if (!response.error) {
+                              return
+                          }
+                      })
+    
+                    originalResult.onHandle();
+                }
+            }
+        } else {
+            // returns original function if type != restaurants
+            return {
+                ...originalResult,
+                onHandle: () => {
+                    originalResult.onHandle()
+                }
+            }
+        }
     }
-    return BetterAction
-  }
+
+    return BetterAction;
+}
