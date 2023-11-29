1491bcb38a0cdfb2fd965a733b61bbff3d334c7a
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-11-29 13:19:15 -0500

    Update page.tsx

--- app/(admin)/admin/(auth)/inventory/page.tsx
+++ app/(admin)/admin/(auth)/inventory/page.tsx
@@ -9,7 +9,7 @@ export default async function InventoryPage() {
     return (
         <div className="p-4 w-full space-y-2">
             <h1 className="text-2xl font-bold">Inventory</h1>
-            <div className='grid grid-cols-4 gap-4'>
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
                 {products?.map((item: any) => (
                     <InventoryCard key={item.id} {...item} />
                 ))}
