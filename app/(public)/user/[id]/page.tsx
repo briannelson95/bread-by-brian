a908690dcb518054eb1f0b9abed53614397aca99
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2024-03-03 10:41:10 -0500

    loyalty card UI

--- app/(public)/user/[id]/page.tsx
+++ app/(public)/user/[id]/page.tsx
@@ -1,4 +1,6 @@
 "use client"
+import MainButton from '@/components/MainButton';
+import PunchCard from '@/components/PunchCard';
 import { supabase } from '@/supabase/lib/supabaseClient';
 import { useSession } from '@supabase/auth-helpers-react';
 import { redirect, usePathname, useRouter } from 'next/navigation';
@@ -11,6 +13,7 @@ export default  function ProfilePage() {
     const router = useRouter();
 
     const [userProfile, setUserProfile]: any = useState();
+    const [punches, setPunches] = useState<number>(0);
 
     useEffect(() => {
         if(session?.user.id !== id) {
@@ -23,6 +26,7 @@ export default  function ProfilePage() {
             .then(result => {
                 if (result.data?.length) {
                     setUserProfile(result.data[0]);
+                    setPunches(result.data[0].punch)
                 }
             })
     }, [])
@@ -35,15 +39,41 @@ export default  function ProfilePage() {
                 Loading...
             </div>
         )
-    }
+    };
+
+    const handlePunch = () => {
+        supabase.from('profiles')
+            .select('punch')
+            .eq('id', userProfile.id)
+            .then(result => {
+                if (!result.error) {
+                    setPunches(result.data[0].punch)
+
+                    supabase.from('profiles')
+                        .update({
+                            punch: result.data[0].punch + 1
+                        })
+                        .eq('id', userProfile.id)
+                        .select()
+                        .then(result => {
+                            console.log(result)
+                        })
+                }
+            })
+    };
 
     return (
-        <div className='w-full p-2 relative mb-16 md:max-w-4xl mx-auto'>
+        <div className='w-full p-2 relative mb-16 md:max-w-4xl mx-auto space-y-6'>
             <div className="flex justify-between">
-                <h1 className='text-3xl font-bold'>Welcome, {userProfile?.full_name}!</h1>
-                <button onClick={async () => {await supabase.auth.signOut(); router.push('/')}}>Sign Out</button>
+                <h1 className='text-3xl font-bold'>Welcome, {userProfile?.full_name}</h1>
+                <button className='text-gray-400 text-sm' onClick={async () => {await supabase.auth.signOut(); router.push('/')}}>Sign Out</button>
             </div>
-           
+            <div className=''>
+                <PunchCard
+                    name={userProfile?.full_name}
+                    punches={punches}
+                />
+            </div>
         </div>
     )
 }
---
+++ components/PunchCard.tsx
@@ -0,0 +1,69 @@
+"use client"
+import Image from 'next/image'
+import React, { useState } from 'react'
+import InfoIcon from './icons/InfoIcon';
+import XIcon from './icons/XIcon';
+
+type PunchCardProps = {
+    name: string;
+    punches: number;
+}
+
+export default function PunchCard({ name, punches }: PunchCardProps) {
+    const [showInfo, setShowInfo] = useState<boolean>(false);
+
+    const handleShowInfo = () => {
+        setShowInfo(!showInfo)
+    }
+    return (
+        <div className='w-full h-full relative'>
+            <div className={`absolute ${showInfo ? 'block' : 'hidden'} right-0 w-3/12 rounded-xl bg-brand-secondary/50 p-2`}>
+                <div className='bg-white w-full rounded-lg p-2'>
+                    <div className='flex justify-between items-start'>
+                        <h3 className='font-medium'>
+                            What is the Loyalty Card?
+                        </h3>
+                        <button onClick={handleShowInfo}>
+                            <XIcon />
+                        </button>
+                    </div>
+                    <p>Every time you purchase a bread item, you will recieve 1 punch on your card. When you have 10 punches you earn 1 FREE Soughdough Loaf.</p>
+                </div>
+            </div>
+            <div className='w-3/4 md:w-1/3 mx-auto bg-zinc-200 aspect-[3/4] rounded-xl px-3 py-2 pb-8 flex flex-col justify-between shadow-md shadow-brand-secondary/40'>
+                <div>
+                    <div className='w-full flex justify-between items-center'>
+                        <h2 className='text-xl font-semibold flex gap-1 items-center'>
+                            Loyalty Card
+                            <button onClick={handleShowInfo}>
+                                <InfoIcon />
+                            </button>
+                        </h2>
+                        <Image
+                            src={'/bread-logo.png'}
+                            height={1000}
+                            width={1000}
+                            alt='logo'
+                            className='h-12 w-12'
+                        />
+                    </div>
+                    {name}
+                    <p>Buy 10 breads <br /> Get 1 free</p>
+                </div>
+                <div className='grid gap-4 grid-cols-5 relative'>
+                    {Array.from({ length: 10 }, (_, index) => (
+                        <div key={index} className='border-brand-secondary bg-brand-secondary/30 border rounded-full w-8 h-8 relative' />
+                    ))}
+                    <div className='w-full grid gap-4 grid-cols-5 absolute z-10 place-items-center'>
+                        {Array.from({ length: punches}, (_, index) => (
+                            <div key={index} className='flex items-center justify-center text-lg'>
+                                👍
+                            </div>
+                        ))}
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
