e97ebc9431b10b8db55979f79495e3de1355767c
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2024-03-18 09:08:58 -0400

    fixed admin

--- app/(admin)/admin/(auth)/layout.tsx
+++ app/(admin)/admin/(auth)/layout.tsx
@@ -1,16 +1,20 @@
+"use client"
 import AdminNav from '@/components/admin/AdminNav'
-import { UserContextProvider } from '@/context/UserContext'
-import React from 'react'
+import { UserContext, UserContextProvider } from '@/context/UserContext'
+import { redirect } from 'next/navigation'
+import React, { useContext } from 'react'
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
+    const {profile}: any = useContext(UserContext)
+    if (!profile?.admin) {
+        redirect('/')
+    }
     return (
-        // <UserContextProvider>
-            <main className='flex min-h-screen md:pb-10 bg-gray-100'>
-                <AdminNav />
-                <div className='md:ml-52 w-full relative'>
-                    {children}
-                </div>
-            </main>
-        // </UserContextProvider>
+        <main className='flex min-h-screen md:pb-10 bg-gray-100'>
+            <AdminNav />
+            <div className='md:ml-52 w-full relative'>
+                {children}
+            </div>
+        </main>
     )
 }
